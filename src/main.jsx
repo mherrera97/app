a64749f8d7ddcb5fb522f4b95175c5338c391205
mherrera97 <mherrera@clinicasomer.com>
2023-02-20 20:24:20 -0500

    pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { store } from "./store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="http://192.168.1.136/">
       <GestorApp />
     </BrowserRouter>
   </Provider>
