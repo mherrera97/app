1299b845b309eaf7a1ea29d3db9d5e8bbb16acbc
mherrera97 <mherrera@clinicasomer.com>
2023-02-20 19:57:35 -0500

    pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { GestorApp } from "./GestorApp";
-import "./styles.css";
 import { Provider } from "react-redux";
 
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <GestorApp />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <GestorApp />
+    </BrowserRouter>
+  </Provider>
+  // </React.StrictMode>
 );
