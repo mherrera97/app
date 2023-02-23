7eb8c6bf2ad0e1c68930025769b9f91e9222112a
mherrera97 <mherrera@clinicasomer.com>
2023-02-23 16:05:22 -0500

    cambio en la ruta del api

--- src/config/front.env.js
+++ src/config/front.env.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:4000/';
-export const API_URL = import.meta.env.VITE_API_URL??window._env_?.API_URL??'https://192.168.1.72/v1';
+export const BASE_URL = 'http://localhost:5173/';
+export const API_URL = import.meta.env.VITE_API_URL??window._env_?.API_URL??'http://192.168.1.72/v1';
