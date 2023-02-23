adf9621246895e83154abfe96aa0dc4fea494768
mherrera97 <mherrera@clinicasomer.com>
2023-02-23 15:11:42 -0500

    pruebas

--- src/config/front.env.js
+++ src/config/front.env.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'http://localhost:4000/';
-export const API_URL = import.meta.env.VITE_API_URL??window._env_?.API_URL??'https://localhost/v1/';
+export const API_URL = import.meta.env.VITE_API_URL??window._env_?.API_URL??'https://192.168.1.72/v1';
