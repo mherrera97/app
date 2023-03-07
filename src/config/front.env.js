0eb0ce29fe20f2dca7271a3490e82e1b06d94e10
mherrera97 <mherrera@clinicasomer.com>
2023-03-07 15:38:56 -0500

    cambio de la url del api

--- src/config/front.env.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'http://localhost:5173/';
-export const API_URL = import.meta.env.VITE_API_URL??window._env_?.API_URL??'http://192.168.1.72/v1';
--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const replacePlugin = (mode)=>{
-  return {
-    name:"html-inject-env",
-    transformIndexHtml:(html)=>{
-      if(mode==="production"){
-        return html.replace(
-          "<!-- REACT_ENV -->",
-          `<script src="/config/front.env.js"></script>`
-        );
-      }
-      return null;
-    }
-  };
-};
+// const replacePlugin = (mode)=>{
+//   return {
+//     name:"html-inject-env",
+//     transformIndexHtml:(html)=>{
+//       if(mode==="production"){
+//         return html.replace(
+//           "<!-- REACT_ENV -->",
+//           `<script src="/config/front.env.js"></script>`
+//         );
+//       }
+//       return null;
+//     }
+//   };
+// };
 
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode})=>({
-  plugins: [react(),replacePlugin(mode)],
+  plugins: [react()],
 }));
