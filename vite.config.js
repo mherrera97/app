3b2b153edb2ea573a624b8c8a0386fc2a3f3995c
mherrera97 <mherrera@clinicasomer.com>
2023-03-07 16:12:20 -0500

    cambios de configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // const replacePlugin = (mode)=>{
 //   return {
@@ -17,8 +16,7 @@ import react from '@vitejs/plugin-react'
 //   };
 // };
 
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode})=>({
+export default defineConfig({
   plugins: [react()],
-}));
+});
